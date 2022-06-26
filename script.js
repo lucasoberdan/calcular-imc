cb0ad8961310cfd3e47dfef4db1964f8e60f3b10
Lucas Oberdan <74725641+lucasoberdan@users.noreply.github.com>
2022-06-25 21:53:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const elementoResposta = document.querySelector("#resposta");
+const elementoPesoIdeal = document.querySelector("#retornaPesoIdeal");
+
+function imc() {
+  var elementoPeso = document.getElementById("valorPeso");
+  var elementoAltura = document.getElementById("valorAltura");
+
+  var peso = elementoPeso.value;
+  var altura = elementoAltura.value;
+
+  var calculoImc = peso / (altura * altura);
+
+  if (calculoImc <= 18.5) {
+    elementoResposta.innerHTML = `Seu IMC é ${calculoImc.toFixed(
+      1
+    )} - Baixo Peso.`;
+  } else if (calculoImc >= 18.6 && calculoImc <= 24.99) {
+    elementoResposta.innerHTML = `Seu IMC é ${calculoImc.toFixed(1)} - Normal.`;
+  } else if (calculoImc >= 25 && calculoImc <= 29.99) {
+    elementoResposta.innerHTML = `Seu IMC é ${calculoImc.toFixed(
+      1
+    )} - Sobrepeso.`;
+  } else if (calculoImc >= 30) {
+    elementoResposta.innerHTML = `Seu IMC é ${calculoImc.toFixed(
+      1
+    )} - Obesidade.`;
+  }
+
+  var menorPeso = (peso * 18.6) / calculoImc;
+  var maiorPeso = (peso * 24.99) / calculoImc;
+
+  elementoPesoIdeal.innerHTML = `Seu peso ideal está entre ${menorPeso.toFixed(
+    1
+  )}kg e ${maiorPeso.toFixed(1)}kg.`;
+}
